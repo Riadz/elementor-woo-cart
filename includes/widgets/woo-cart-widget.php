f8ed8052eabb0cf50cbcce476ec1344ffb7858b6
Riadz <riad.hachemane@gmail.com>
2020-11-23 19:45:15 +0000

    added cart render function

--- includes/widgets/woo-cart-widget.php
+++ includes/widgets/woo-cart-widget.php
@@ -1,20 +1,39 @@
 <?php
 class Woocommerce_Cart_Widget extends \Elementor\Widget_Base
 {
-	public function get_name()
+	function get_name()
 	{
-		return 'woo-cart';
+		return 'woo-cart-elementor-llosqld';
 	}
-	public function get_title()
+	function get_title()
 	{
 		return 'Woocommerce Cart Widget';
 	}
-	public function get_icon()
+	function get_icon()
 	{
 		return 'fas fa-cart-plus';
 	}
-	public function get_categories()
+	function get_categories()
 	{
 		return ['general'];
+	}
+
+	function render()
+	{ ?>
+
+		<div class="woo-cart">
+			<!-- Icon -->
+			<a class="woo-cart-icon" href="<?= wc_get_cart_url() ?>">
+				<i class="fas fa-shopping-cart"></i>
+				<span class="count"><?= wc()->cart->get_cart_contents_count() ?></span>
+			</a>
+
+			<!-- Dropdown -->
+			<div class="woo-cart-dropdown">
+				<?php woocommerce_mini_cart() ?>
+			</div>
+		</div>
+
+<?php
 	}
 }
