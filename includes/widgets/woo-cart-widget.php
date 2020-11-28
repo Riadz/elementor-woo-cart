e0535ab76dd9e07f6df6248932f0be856a347f16
Riadz <riad.hachemane@gmail.com>
2020-11-28 18:27:45 +0000

    added content control

--- includes/widgets/woo-cart-widget.php
+++ includes/widgets/woo-cart-widget.php
@@ -3,7 +3,7 @@ class Woocommerce_Cart_Widget extends \Elementor\Widget_Base
 {
 	function get_name()
 	{
-		return 'woo-cart-elementor-llosqld';
+		return 'woo-cart-elementor';
 	}
 	function get_title()
 	{
@@ -18,14 +18,89 @@ class Woocommerce_Cart_Widget extends \Elementor\Widget_Base
 		return ['general'];
 	}
 
-	function render()
-	{ ?>
+	function _register_controls()
+	{
+		// Content
+		$this->start_controls_section(
+			'content_section',
+			[
+				'label' => __('Content'),
+				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);
 
-		<div class="woo-cart basic">
+		$this->add_control(
+			'cart_style',
+			[
+				'label'   => __('Cart Style'),
+				'type'    => \Elementor\Controls_Manager::SELECT,
+				'options' => [
+					'basic'  => __('Basic'),
+					'simple' => __('Simple'),
+				],
+				'default' => 'basic',
+			]
+		);
+		$this->add_control(
+			'icon',
+			[
+				'label' => __('Cart Icon'),
+				'type' => \Elementor\Controls_Manager::ICONS,
+			]
+		);
+
+		$this->end_controls_section();
+	}
+
+	function render()
+	{
+		$settings = $this->get_settings_for_display();
+
+		$icon = $settings['icon'];
+		$style = $settings['cart_style'];
+		?>
+		<div class="woo-cart <?= $style ?>">
 			<!-- Icon -->
 			<a class="woo-cart-icon" href="<?= wc_get_cart_url() ?>">
-				<i class="fas fa-shopping-cart"></i>
+				<?php if ($icon['library'] != 'svg') : ?>
+					<i class="<?= $icon['value'] ?>"></i>
+				<?php else : ?>
+					<img src="<?= $icon['value']['url'] ?>" alt="">
+				<?php endif ?>
+
 				<span class="count"><?= wc()->cart->get_cart_contents_count() ?></span>
+			</a>
+
+			<!-- Dropdown -->
+			<div class="woo-cart-dropdown">
+				<div class="woo-cart-header">
+					<span>Cart</span>
+					<a href="<?= wc_get_cart_url() ?>">
+						<i class="fas fa-external-link-alt"></i>
+					</a>
+				</div>
+				<div class="woo-cart-content">
+					<?php woocommerce_mini_cart() ?>
+				</div>
+			</div>
+		</div>
+
+	<?php
+		}
+
+		function _content_template()
+		{ ?>
+
+		<div class="woo-cart {{{ settings.cart_style }}}">
+			<!-- Icon -->
+			<a class="woo-cart-icon" href="<?= wc_get_cart_url() ?>">
+				<# if(settings.icon.library !='svg' ) { #>
+					<i class="{{{ settings.icon.value }}}"></i>
+					<# } else { #>
+						<img src="{{{ settings.icon.value.url }}}" alt="">
+						<# } #>
+
+							<span class="count"><?= wc()->cart->get_cart_contents_count() ?></span>
 			</a>
 
 			<!-- Dropdown -->
