c1c8556d64bd03b624b09d36ef5466c2df880a51
Riadz <riad.hachemane@gmail.com>
2020-11-30 21:05:52 +0000

    added controls and styles

--- includes/widgets/woo-cart-widget.php
+++ includes/widgets/woo-cart-widget.php
@@ -50,6 +50,161 @@ class Woocommerce_Cart_Widget extends \Elementor\Widget_Base
 		);
 
 		$this->end_controls_section();
+
+		// Style
+		$this->start_controls_section(
+			'section_style_icon',
+			[
+				'label' => __('Cart Icon'),
+				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_responsive_control(
+			'size',
+			[
+				'label' => __('Size'),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'range' => [
+					'px' => [
+						'min' => 10,
+						'max' => 200,
+					],
+				],
+				'default' => [
+					'size' => 30,
+				],
+				'size_units' => ['px'],
+				'selectors' => [
+					'{{WRAPPER}} .woo-cart-icon' => 'font-size: {{SIZE}}{{UNIT}}',
+				],
+			]
+		);
+
+		$this->start_controls_tabs('tab_icon_color');
+		$this->start_controls_tab('icon_color_normal', ['label' => __('Normal')]);
+		$this->add_control(
+			'icon_color',
+			[
+				'label'     => __('Color'),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'default'   => '#000',
+				'selectors' => [
+					'{{WRAPPER}} .woo-cart-icon' => 'color: {{VALUE}}',
+				],
+			]
+		);
+		$this->end_controls_tab();
+
+		$this->start_controls_tab('icon_color_hover', ['label' => __('Hover')]);
+		$this->add_control(
+			'icon_color:hover',
+			[
+				'label'     => __('Color'),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .woo-cart-icon:hover' => 'color: {{VALUE}}',
+				],
+			]
+		);
+		$this->end_controls_tab();
+		$this->end_controls_tabs();
+
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'section_style_count',
+			[
+				'label' => __('Cart Item Count'),
+				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_responsive_control(
+			'count_left',
+			[
+				'label' => __('X axis'),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => ['%', 'px'],
+				'range' => [
+					'%' => [
+						'min' => -100,
+						'max' => 200,
+					],
+					'px' => [
+						'min' => -200,
+						'max' => 200,
+					],
+				],
+				'selectors' => [
+					'{{WRAPPER}} .woo-cart-icon .count' => 'left: {{SIZE}}{{UNIT}}',
+				],
+			]
+		);
+		$this->add_responsive_control(
+			'count_top',
+			[
+				'label' => __('Y axis'),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => ['%', 'px'],
+				'default' => ['unit' => '%'],
+				'range' => [
+					'%' => [
+						'min' => -100,
+						'max' => 200,
+					],
+					'px' => [
+						'min' => -200,
+						'max' => 200,
+					],
+				],
+				'selectors' => [
+					'{{WRAPPER}} .woo-cart-icon .count' => 'top: {{SIZE}}{{UNIT}}',
+				],
+			]
+		);
+		$this->add_control(
+			'count_color',
+			[
+				'label'     => __('Number Color'),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'default' => '#fff',
+				'selectors' => [
+					'{{WRAPPER}} .woo-cart-icon .count' => 'color: {{VALUE}}',
+				],
+			]
+		);
+		$this->add_control(
+			'count_bg_color',
+			[
+				'label'     => __('Background Color'),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'default' => '#d9534f',
+				'selectors' => [
+					'{{WRAPPER}} .woo-cart-icon .count' => 'background-color: {{VALUE}}',
+				],
+			]
+		);
+		$this->add_responsive_control(
+			'count_size',
+			[
+				'label' => __('Size'),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => ['px'],
+				'default' => ['size' => '10'],
+				'range' => [
+					'px' => [
+						'min' => 8,
+						'max' => 30,
+					],
+				],
+				'selectors' => [
+					'{{WRAPPER}} .woo-cart-icon .count' => 'font-size: {{SIZE}}{{UNIT}}',
+				],
+			]
+		);
+
+		$this->end_controls_section();
 	}
 
 	function render()
